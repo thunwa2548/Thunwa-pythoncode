3c6766ed6b34607608349810293598da5eb3ccf7
Thunwa Wattanapiromsakul <Thunwa.w@ku.th>
2025-07-15 11:58:35 +0700

    lab2.1

---
+++ assignments/week02/lab01.py
@@ -0,0 +1,33 @@
+"""
+BMI Calculator (20 points)
+
+Write a program that:
+
+Asks for weight in kilograms
+Asks for height in meters
+Calculates BMI using formula: BMI = weight / (height²)
+Displays BMI with 1 decimal place
+Shows BMI category based on the ranges below
+
+BMI Categories:
+
+Below 18.5: Underweight
+18.5 - 24.9: Normal weight
+25.0 - 29.9: Overweight
+30.0 and above: Obese
+
+"""
+
+weight=float(input("Weight:"))
+height =float(input("Height: "))
+bmi=weight/height**2
+
+if bmi<18.5:
+    print("Underweight")
+    if bmi>=18.5 and bmi <=24.9:
+        print("Normal weight")
+        if bmi >=25.0 and bmi<=29.9:
+            print("Overweight")
+            if bmi >=30.0: 
+                print("obese")
+                print("Your bmi is",bmi)
